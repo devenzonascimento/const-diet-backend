586abe00cb7c4c6ed5ef54ec92cb71c8bbe5fa9c
devenzonascimento <dev.enzody@gmail.com>
2024-06-05 18:57:47 -0300

    feat: add user use case (initial)

---
+++ src/usecases/user-usecase.ts
@@ -0,0 +1,99 @@
+import { UserRepositoryPrisma } from "../repositories/user-repository.js";
+
+import jwt from "jsonwebtoken";
+import { compareSync, hashSync } from "bcrypt";
+
+import {
+  User,
+  UserCreate,
+  UserLogin,
+  UserRepository,
+  UserStats,
+  UserUpdate,
+} from "../interfaces/user-interface.js";
+
+interface DecodedToken extends jwt.JwtPayload {
+  email: string;
+}
+
+export class UserUseCase {
+  private userRepository: UserRepository;
+
+  constructor() {
+    this.userRepository = new UserRepositoryPrisma();
+  }
+
+  async create({ name, email, password }: UserCreate): Promise<User> {
+    const userExists = await this.userRepository.findByEmail(email);
+
+    if (userExists) {
+      throw new Error("User already exists.");
+    }
+
+    password = hashSync(password, 10);
+
+    return await this.userRepository.create({ name, email, password });
+  }
+
+  async login({ email, password }: UserLogin) {
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("User not found.");
+    }
+
+    const isCorrectPassword = compareSync(password, user.password);
+
+    if (!isCorrectPassword) {
+      throw new Error("Invalid password.");
+    }
+
+    const jwtKey = process.env.JWT_KEY;
+    if (!jwtKey) {
+      throw new Error("JWT key is not defined.");
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        password: user.password,
+      },
+      jwtKey,
+      { expiresIn: 3600 }
+    );
+
+    return { userId: user.id, token };
+  }
+
+  async verifyToken(token: string) {
+    try {
+      const jwtKey = process.env.JWT_KEY;
+      if (!jwtKey) {
+        throw new Error("JWT key is not defined.");
+      }
+
+      const decodedToken = jwt.verify(token, jwtKey) as DecodedToken;
+
+      return await this.userRepository.findByEmail(decodedToken.email);
+    } catch (error) {
+      throw new Error(`Error verifying token: ${error}`);
+    }
+  }
+
+  async findByEmail(email: string) {
+    return await this.userRepository.findByEmail(email);
+  }
+
+  async addStats(userId: string, data: UserStats) {
+    return await this.userRepository.addStats(userId, data)
+  }
+
+  async update(userId: string, data: UserUpdate) {
+    return await this.userRepository.update(userId, data);
+  }
+
+  async delete(userId: string) {
+    this.userRepository.delete(userId);
+  }
+}
