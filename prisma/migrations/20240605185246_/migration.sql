4fa5beec087dbcf6a464e88a687b15c00fa0d2bd
devenzonascimento <dev.enzody@gmail.com>
2024-06-05 16:15:00 -0300

    chore: update prisma connection to database

---
+++ prisma/migrations/20240605185246_/migration.sql
@@ -0,0 +1,129 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('PENDING', 'COMPLETED', 'MISSED');
+
+-- CreateEnum
+CREATE TYPE "Unit" AS ENUM ('GRAMS', 'MILILITERS', 'UNITS');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "age" INTEGER,
+    "height" INTEGER,
+    "weight" DOUBLE PRECISION,
+    "sex" TEXT,
+    "activityLevel" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Plan" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "goal" TEXT NOT NULL,
+
+    CONSTRAINT "Plan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Routine" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Routine_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "DailyRoutine" (
+    "planId" TEXT NOT NULL,
+    "routineId" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'PENDING',
+
+    CONSTRAINT "DailyRoutine_pkey" PRIMARY KEY ("planId","routineId")
+);
+
+-- CreateTable
+CREATE TABLE "DailyMeal" (
+    "routineId" TEXT NOT NULL,
+    "mealId" TEXT NOT NULL,
+    "scheduledTime" TIMESTAMP(3) NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'PENDING',
+
+    CONSTRAINT "DailyMeal_pkey" PRIMARY KEY ("routineId","mealId")
+);
+
+-- CreateTable
+CREATE TABLE "Meal" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Meal_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FoodInMeal" (
+    "quantity" DOUBLE PRECISION NOT NULL,
+    "unit" "Unit" NOT NULL DEFAULT 'GRAMS',
+    "foodId" TEXT NOT NULL,
+    "mealId" TEXT NOT NULL,
+
+    CONSTRAINT "FoodInMeal_pkey" PRIMARY KEY ("mealId","foodId")
+);
+
+-- CreateTable
+CREATE TABLE "Food" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "carbohydrates" DOUBLE PRECISION NOT NULL,
+    "proteins" DOUBLE PRECISION NOT NULL,
+    "fats" DOUBLE PRECISION NOT NULL,
+    "sodiums" DOUBLE PRECISION NOT NULL,
+    "fibers" DOUBLE PRECISION NOT NULL,
+    "calories" DOUBLE PRECISION NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Food_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Plan" ADD CONSTRAINT "Plan_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Routine" ADD CONSTRAINT "Routine_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DailyRoutine" ADD CONSTRAINT "DailyRoutine_planId_fkey" FOREIGN KEY ("planId") REFERENCES "Plan"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DailyRoutine" ADD CONSTRAINT "DailyRoutine_routineId_fkey" FOREIGN KEY ("routineId") REFERENCES "Routine"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DailyMeal" ADD CONSTRAINT "DailyMeal_routineId_fkey" FOREIGN KEY ("routineId") REFERENCES "Routine"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DailyMeal" ADD CONSTRAINT "DailyMeal_mealId_fkey" FOREIGN KEY ("mealId") REFERENCES "Meal"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Meal" ADD CONSTRAINT "Meal_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FoodInMeal" ADD CONSTRAINT "FoodInMeal_foodId_fkey" FOREIGN KEY ("foodId") REFERENCES "Food"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FoodInMeal" ADD CONSTRAINT "FoodInMeal_mealId_fkey" FOREIGN KEY ("mealId") REFERENCES "Meal"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Food" ADD CONSTRAINT "Food_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
