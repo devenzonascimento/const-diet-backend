3fd61430c77fc76ae05d7b71919e50af9a650a55
devenzonascimento <dev.enzody@gmail.com>
2024-06-06 14:06:09 -0300

    feat: add all interfaces of meal food types and methods

--- src/interfaces/meal-food-interface.ts
+++ src/interfaces/meal-food-interface.ts
@@ -1,9 +1,40 @@
-import { $Enums } from "@prisma/client";
-
+import { Prisma } from "@prisma/client";
 import { Food } from "./food-interface";
 
 export interface MealFood {
   food: Food
   quantity: number
-  unit: $Enums.Unit
+}
+
+export interface MealFoodComplete {
+  mealId: string;
+  foodId: string;
+  quantity: number;
+}
+
+export interface MealFoodCreate {
+  foodId: string;
+  quantity: number
+}
+
+export interface MealFoodUpdate {
+  foodsToCreate: {
+    foodId: string;
+    quantity: number;
+  }[]
+  foodsToUpdate: {
+    foodId: string;
+    quantity: number;
+  }[]
+  foodsToDelete: {
+    foodId: string;
+  }[]
+}
+
+export interface MealFoodRepository {
+  createMany: (mealId: string, data: MealFoodCreate[]) => Promise<Prisma.BatchPayload>
+  findMany: (mealId: string) => Promise<MealFoodComplete[]>
+  getAllFoodsByMealId: (mealId: string) => Promise<MealFood[]>
+  update: (mealId: string, foods: MealFoodUpdate) => Promise<void>
+  delete: (mealId: string, foodId: string) => Promise<void>
 }
