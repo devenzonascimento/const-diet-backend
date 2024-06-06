e4e629ae780dd9e5a7dbd43879681e1b6a2d3c42
devenzonascimento <dev.enzody@gmail.com>
2024-06-06 14:11:38 -0300

    feat: add link to meal food route on the server

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import { fastifyCors } from "@fastify/cors";
 
 import { userRoutes } from "./routes/user-route";
 import { foodRoutes } from "./routes/food-route";
+import { mealRoutes } from "./routes/meal-route";
+import { authMiddleware } from "./middlewares/auth-middleware";
 
 const server: FastifyInstance = fastify();
 
@@ -14,17 +16,12 @@ server.get("/", () => {
   return "SERVER ON!";
 });
 
+server.get("/auth", {preHandler: authMiddleware}, (_, reply) => {
+  reply.code(200).send()
+});
+
 server.register(userRoutes, {
   prefix: "/users",
-});
-
-server.register(foodRoutes, {
-  prefix: "users/:userId/foods",
-});
-
-/*
-server.get("/auth", {preHandler: authMiddleware}, (req, reply) => {
-  reply.code(200).send()
 });
 
 server.register(foodRoutes, {
@@ -34,6 +31,13 @@ server.register(foodRoutes, {
 server.register(mealRoutes, {
   prefix: "users/:userId/meals",
 });
+
+/*
+server.register(foodRoutes, {
+  prefix: "users/:userId/foods",
+});
+
+
 
 server.register(rotineRoutes, {
   prefix: "/routines",
