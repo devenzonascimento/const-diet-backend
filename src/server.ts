33c5f04608b191ee5cf45dd30cbd47b275ba10d1
devenzonascimento <dev.enzody@gmail.com>
2024-06-05 22:17:59 -0300

    feat: add links to user and food routes on the server

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,55 @@
-import fastify from "fastify";
+import { fastify, FastifyInstance } from "fastify";
 import fastifyCors from "@fastify/cors";
 
-const server = fastify();
+import { userRoutes } from "./routes/user-route";
+import { foodRoutes } from "./routes/food-route";
+
+const server: FastifyInstance = fastify();
 
 server.register(fastifyCors, {
-  origin: true
-})
-
-server.get("/", () => {
-  return "SERVER ON!!!";
+  origin: true,
 });
 
-server.listen(
-  {
-    port: 3333,
-  },
-  () => console.log("SERVIDOR ONLINE")
-);
+server.get("/", () => {
+  return "SERVER ON!";
+});
+
+server.register(userRoutes, {
+  prefix: "/users",
+});
+
+server.register(foodRoutes, {
+  prefix: "users/:userId/foods",
+});
+
+/*
+server.get("/auth", {preHandler: authMiddleware}, (req, reply) => {
+  reply.code(200).send()
+});
+
+server.register(foodRoutes, {
+  prefix: "users/:userId/foods",
+});
+
+server.register(mealRoutes, {
+  prefix: "users/:userId/meals",
+});
+
+server.register(rotineRoutes, {
+  prefix: "/routines",
+});
+
+server.register(foodInMealRoutes, {
+  prefix: "/users/:userId/meals/:mealId/foods",
+});
+
+server.register(dailyMealRoutes, {
+  prefix: "/users/daily-meals",
+});
+
+server.register(planRoutes, {
+  prefix: "/users/plan",
+});
+*/
+
+server.listen({ port: 3333 }, () => console.log("SERVIDOR ONLINE"));
