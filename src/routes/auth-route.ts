4de0a71b36e2bce7cbbb9369e2a5361613b42a86
devenzonascimento <dev.enzody@gmail.com>
2024-06-07 15:27:57 -0300

    feat: add route to verify authentication and refresh token

---
+++ src/routes/auth-route.ts
@@ -0,0 +1,23 @@
+import { FastifyInstance } from "fastify";
+
+import { authMiddleware } from "../middlewares/auth-middleware.js";
+
+import { UserUseCase } from "../usecases/user-usecase.js";
+
+export const authRoutes = async (server: FastifyInstance) => {
+  const userUseCase = new UserUseCase()
+
+  server.addHook("preHandler", authMiddleware);
+
+  server.get("/", async (req, reply) => {
+    try {
+      const token = req.headers.authorization?.replace(/^Bearer /, "") as string;
+
+      const newToken = await userUseCase.refreshToken(token)
+
+      reply.code(200).send(newToken);
+    } catch (error) {
+      reply.code(500).send(error);
+    }
+  });
+};
