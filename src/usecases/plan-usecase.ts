a42e8229ade5a527bf701c70f9869c725ccf2f9b
devenzonascimento <dev.enzody@gmail.com>
2024-07-05 23:51:37 -0300

    feat: add plan use case (initial)

---
+++ src/usecases/plan-usecase.ts
@@ -0,0 +1,13 @@
+import { PlanCreate } from "../interfaces/plan-interface.js";
+import { PlanRepositoryPrisma } from "../repositories/plan-repository.js";
+
+export class PlanUseCase {
+  private planRepository;
+
+  constructor() {
+    this.planRepository = new PlanRepositoryPrisma();
+  }
+  async create(data: PlanCreate) {
+    return await this.planRepository.create(data);
+  }
+}
