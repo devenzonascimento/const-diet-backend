f1554681e79dbd874033bbf4404505420e2494c0
devenzonascimento <dev.enzody@gmail.com>
2024-07-29 17:55:41 -0300

    feat: implements a operation to recalculate routine calculated fields when a meal was updated or deleted

--- src/usecases/meal-usecase.ts
+++ src/usecases/meal-usecase.ts
@@ -1,24 +1,30 @@
 import { MealRepositoryPrisma } from "../repositories/meal-repository.js";
 import { MealFoodRepositoryPrisma } from "../repositories/meal-food-repository.js";
+import { RoutineMealRepositoryPrisma } from "../repositories/routine-meal-repository.js";
+
+import { calculateFieldsBasedOnFoods } from "../functions/calculate-fields-based-on-foods.js";
+import { RoutineUseCase } from "./routine-usecase.js";
 
 import { MealCreate, MealUpdate } from "../interfaces/meal-interface.js";
 import { MealFoodCreate } from "../interfaces/meal-food-interface.js";
 
-import { calculateFieldsBasedOnFoods } from "../functions/calculate-fields-based-on-foods.js";
-
 export class MealUseCase {
   private mealRepository;
   private mealFoodRepository;
+  private routineMealRepository;
+  private routineUseCase;
 
   constructor() {
     this.mealRepository = new MealRepositoryPrisma();
     this.mealFoodRepository = new MealFoodRepositoryPrisma();
+    this.routineMealRepository = new RoutineMealRepositoryPrisma();
+    this.routineUseCase = new RoutineUseCase();
   }
 
   async create(mealData: MealCreate, foodsData: MealFoodCreate[]) {
     const { id } = await this.mealRepository.create(mealData, foodsData);
 
-    const meal = await this.saveCalculatedFields(id)
+    const meal = await this.saveCalculatedFields(id);
 
     return meal;
   }
@@ -32,7 +38,6 @@ export class MealUseCase {
   }
 
   async update(mealData: MealUpdate, foodsData: MealFoodCreate[]) {
-    
     const currentFoods = await this.mealFoodRepository.findMany(mealData.id);
 
     const currentFoodIds = currentFoods.map((food) => food.foodId);
@@ -56,20 +61,44 @@ export class MealUseCase {
       foodsToDelete,
     });
 
-    const meal = await this.saveCalculatedFields(mealData.id)
+    const meal = await this.saveCalculatedFields(mealData.id);
+
+    const routinesIdsToRecalculate =
+      await this.routineMealRepository.getRoutineIdsRelatedToMeal(mealData.id);
+
+    this.recalculateFieldsOnFoodUpdate(routinesIdsToRecalculate);
 
     return meal;
   }
 
   async delete(mealId: string) {
+    const routinesIdsToRecalculate =
+      await this.routineMealRepository.getRoutineIdsRelatedToMeal(mealId);
+
     await this.mealRepository.delete(mealId);
+
+    this.recalculateFieldsOnFoodUpdate(routinesIdsToRecalculate);
   }
 
   async saveCalculatedFields(mealId: string) {
-    const foodsToCalculate = await this.mealFoodRepository.getAllFoodsByMealId(mealId);
+    const foodsToCalculate = await this.mealFoodRepository.getAllFoodsByMealId(
+      mealId
+    );
 
-    const calculatedFields = calculateFieldsBasedOnFoods(foodsToCalculate)
+    const calculatedFields = calculateFieldsBasedOnFoods(foodsToCalculate);
 
-    return await this.mealRepository.saveCalculatedFields(mealId, calculatedFields);
+    return await this.mealRepository.saveCalculatedFields(
+      mealId,
+      calculatedFields
+    );
+  }
+
+  async recalculateFieldsOnFoodUpdate(
+    routinesIdsToRecalculate: { routineId: string }[]
+  ) {
+    routinesIdsToRecalculate.forEach(
+      async ({ routineId }) =>
+        await this.routineUseCase.saveCalculatedFields(routineId)
+    );
   }
 }
