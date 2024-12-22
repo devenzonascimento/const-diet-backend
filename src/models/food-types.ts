ea5645c23bdb0e568410bfdcf9de0607fc179720
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2024-12-22 13:27:54 -0300

    Adiciona campo imageUrl opcional ao modelo Food

--- src/models/food-types.ts
+++ src/models/food-types.ts
@@ -5,6 +5,7 @@ export type UnitType = 'GRAMS' | 'MILILITERS'
 export type Food = {
   id: number
   name: string
+  imageUrl?: string
   unit: UnitType
   calories: number
   macronutrients: Macronutrients
